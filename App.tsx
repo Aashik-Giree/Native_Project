3b07414a2a4fd69cd02b338b1bf08875e606f3e3
Aashik-Giree <ghub282@gmail.com>
2023-08-02 11:11:03 +0545

    second commit

--- App.tsx
+++ App.tsx
@@ -4,98 +4,18 @@
  *
  * @format
  */
-import coffee from './assets/images/coffee.png'
-import cup from './assets/images/cup.png'
-import glass from './assets/images/glass.png'
-import milk from './assets/images/milk.png'
+
 
 // import question from './assets/Data/oneQuestionWithOption'
 import React from 'react';
-import type { PropsWithChildren } from 'react';
-import {
-  Image,
-  Pressable,
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+
+import { Dulingo } from './src/Project/Dulingo';
 
 function App(): JSX.Element {
-
-  const ImageOption = (props:any) => {
-    return (
-      <Pressable onPress={()=>{
-        console.warn(props.text);
-        
-      }}
-      style={style.box} >
-        <Image source={props.img} style={style.img} />
-        <Text style={{ fontSize: 25, fontWeight:'bold', color:'black'}}>{props.text}</Text>
-      </Pressable>
-    )
-  }
-
   return (
     <>
-      <SafeAreaView style={style.container}>
-
-        <View style={style.header}>
-          <Text style={style.text}>Which of these is "the glass"?</Text>
-        </View>
-
-        <View style={style.imgContainer}>           
-           <ImageOption img={cup} text='Cup'/>
-           <ImageOption img={coffee} text='Coffee'/>
-           <ImageOption img={glass} text='Glass'/>
-           <ImageOption img={milk} text='Milk'/>
-        </View>
-
-      </SafeAreaView>      
+      <Dulingo />
     </>
-  );
+  )
 }
-const style = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  header: {
-    width: '100%',
-    height: '15%',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  imgContainer: {
-    width: '100%',
-    height: '85%',
-    padding: 5,
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  box: {
-    width: '50%',
-    height: '50%',
-    backgroundColor: 'white',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderWidth: 2,
-    borderColor: 'red',
-  },
-
-  text: {
-    fontSize: 25,
-    color: 'black',
-    fontWeight: 'bold',
-  },
-  img: {
-    width: '80%',
-    height: '70%',
-    resizeMode: 'contain'
-
-  }
-})
-
 export default App;
---
+++ src/Project/Dulingo/index.tsx
@@ -0,0 +1,115 @@
+import React, {useState} from 'react';
+
+import {View, Text, StyleSheet, Image, Pressable, Alert} from 'react-native';
+import questions from '../../../assets/Data/imageMultipleChoiceQuestion'
+
+const CustomText = (props: any) => {
+  return <Text style={styles.textStyle}>{props.name}</Text>;
+};
+
+const ImageOption = (props: any) => {
+  const {name, isSelected = false, image, handlePress} = props;
+  return (
+    <Pressable
+      onPress={() => {
+        handlePress(name);
+      }}
+      style={[
+        styles.optionContainer,
+        isSelected ? styles.selectedContainer : {},
+      ]}>
+      <Image resizeMode="contain" style={styles.image} source={{uri: image}} />
+      <Text style={isSelected ? styles.selectedText : {}}>{name}</Text>
+    </Pressable>
+  );
+};
+
+// useState, useEffect ,props
+
+export const Dulingo = () => {
+  const [questionIndex, setCurrentIndex] = useState(1);
+  const [selectedOption, setSelectedOption] = useState<any>(null);
+
+  const handlePress = (name: string) => {
+    console.warn(name);
+  };
+  return (
+    <View style={styles.root}>
+      <CustomText name={questions[questionIndex].question} />
+      <View style={styles.optionsContainer}>
+        {questions[questionIndex].options.map(option => {
+          return (
+            <ImageOption
+              name={option.text}
+              image={option.image}
+              handlePress={() => {
+                setSelectedOption(option);
+              }}
+              isSelected={option.id === selectedOption?.id}
+            />
+          );
+        })}
+        {/* {question.options.map(option => {
+          return <ImageOption name={option.text} />;
+        })} */}
+      </View>
+
+      <Pressable
+        disabled={selectedOption == null}
+        onPress={() => {
+          setCurrentIndex(questionIndex + 1);
+          questionIndex + 1;
+        }}
+        style={{
+          backgroundColor: selectedOption == null ? 'grey' : 'green',
+          padding: 10,
+          marginTop: 10,
+          borderRadius: 20,
+        }}>
+        <Text style={{color: 'white', textAlign: 'center', fontSize: 23}}>
+          Check
+        </Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    padding: 10,
+  },
+  optionsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    flexWrap: 'wrap',
+    alignContent: 'space-between',
+    flex: 1,
+  },
+  optionContainer: {
+    borderWidth: 2,
+    width: '48%',
+    height: '48%',
+    borderColor: 'lightgrey',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  image: {
+    width: '100%',
+    flex: 1,
+  },
+  textStyle: {
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  selectedContainer: {
+    backgroundColor: '#81D5fe',
+  },
+  selectedText: {
+    color: '#40BEF7',
+    fontWeight: 'bold',
+  },
+});
